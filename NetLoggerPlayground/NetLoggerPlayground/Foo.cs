fd2c5dd514961abbc43ee38e4bf7b6a1ca0a5229
Bnaya Eshet <bnayanet@hotmail.com>
2019-06-03 15:35:27 +0300

    seed

---
+++ NetLoggerPlayground/NetLoggerPlayground/Foo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetLoggerPlayground
+{
+    public class Foo
+    {
+        private readonly ILogger _logger;
+
+        public Foo(ILogger<Foo> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug("Foo Created");
+        }
+
+        public void Exec()
+        {
+            _logger.LogInformation("Executing");
+        }
+    }
+}
---
+++ NetLoggerPlayground/NetLoggerPlayground/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+// credit: https://www.blinkingcaret.com/2018/02/14/net-core-console-logging/
+
+namespace NetLoggerPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var foo = serviceProvider.GetService<Foo>();
+
+            foo.Exec();
+
+            Console.ReadKey();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // SeriLog
+            Log.Logger = new LoggerConfiguration()
+                    //.Filter.ByIncludingOnly(l => l.Properties.ContainsKey("Some tag"))
+                    .WriteTo.File("main.log")
+                    .CreateLogger();
+
+            // General
+            IConfiguration configuration = new ConfigurationBuilder()
+                                       .AddEnvironmentVariables()
+                                       .Build();
+
+            services
+                    .Configure<LoggerFilterOptions>(options =>
+                    {
+                        options.AddFilter((category, level) => true);
+                        options.Rules.Add(new LoggerFilterRule("Console", "NetLoggerPlayground.Foo", LogLevel.Trace, (provider, category, level) => true));
+                        if (Enum.TryParse(configuration["LOG_LEVEL"], out LogLevel logLevel))
+                            options.MinLevel = logLevel;
+                        else
+                            options.MinLevel = LogLevel.Information;
+                    })
+                    .AddLogging(configure =>
+                            configure.AddConsole())
+                    .AddSingleton<Foo>();
+            services.AddLogging(configure => configure.AddSerilog());
+        }
+    }
+}
